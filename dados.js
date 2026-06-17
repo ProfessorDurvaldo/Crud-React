789f6e5c7a70a45d073ce22455d684b770209b26
Professor Durvaldo <durvaldomarques2@gmail.com>
2026-06-17 13:20:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+// dados.js
+// Array inicial usado no useState(itens) do roteiro de aula.
+// Basta importar e usar: import { tecnologias } from "./dados";
+
+export const tecnologias = [
+  {
+    id: 1,
+    nome: "JavaScript",
+    valor: 0,
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    id: 2,
+    nome: "React",
+    valor: 0,
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+  },
+  {
+    id: 3,
+    nome: "PHP",
+    valor: 0,
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+  },
+];
